d833bcbb326e3b174c2c154f62d14134e9a31632
KCcooldude11 <kaseylightheart@yahoo.com>
2026-03-07 22:34:51 -0700

    invert test

--- src/constants.js
+++ src/constants.js
@@ -73,7 +73,7 @@ export const PROGRESSION = {
 
 export const DEBUG = {
   TEST_START_NEAR_INVERT_ZONE: {
-    ENABLED: true,
+    ENABLED: false,
     START_SCORE: 990,
     START_COLUMNS: 990,
     START_THEME: 2,
